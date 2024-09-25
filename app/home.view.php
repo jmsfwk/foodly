7936d600f904c80ccbd08411a0e0c1e57902cebb
James Fenwick <j.fenwick@me.com>
2024-09-25 21:18:32 +0100

    Update styling of homepage

--- app/home.view.php
+++ app/home.view.php
@@ -1,13 +1,16 @@
 <html lang="en">
 <head>
-    <title>Tempest</title>
+    <title>foodly</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/main.css" rel="stylesheet">
 </head>
-<body class="flex justify-center items-center">
-
-<h1 class="text-5xl font-bold text-[#4f95d1]">Tempest</h1>
-
+<body class="container max-w-screen-md mx-auto bg-zinc-200 text-zinc-900">
+    <nav class="my-2.5">
+        <a href="/" class="font-bold uppercase transition duration-300 hover:text-sky-600 hover:underline focus:text-sky-600 focus:underline">Foodly</a>
+    </nav>
+    <main class="mt-20">
+        <h1 class="text-3xl font-bold">Latest Recipes</h1>
+    </main>
 </body>
 </html>
